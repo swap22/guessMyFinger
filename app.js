42f6a37ac5bc0dddb6594e96fb0c0fbb564cbff5
swap22 <sharma.sswapnil@gmail.com>
2017-05-13 15:14:26 +0530

    random number generation from0 to 5

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 var rand = Math.random();
-    rand =rand*10;
+   // rand =rand*10;
+   rand= rand*5; // as the maximum finger in one hand is 5
+   rand=Math.round(); // converting the floating point number to whole number
