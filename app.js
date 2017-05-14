118a06b678afd68dfd37c6d80b1efd26e057114d
swap22 <sharma.sswapnil@gmail.com>
2017-05-14 15:21:25 +0530

    added functinality to check the number

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-var rand = Math.random();
-   // rand =rand*10;
-   rand= rand*5; // as the maximum finger in one hand is 5
-   rand=Math.round(); // converting the floating point number to whole number
+
+
+   document.getElementById("checkGuess").onclick = function () {
+       var rand = Math.random();
+       // rand =rand*10;
+       rand = rand * 5; // as the maximum finger in one hand is 5
+       rand = Math.round(); // converting the floating point number to whole number
+
+       if (document.getElementById("guess").value == rand) {
+
+           alert("congrats! You got it!");
+
+       } else {
+
+           alert("Nope! The number was " + rand + ". Better try again!");
+
+       }
+
+   }
